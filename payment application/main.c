e6de5ee95801e3835d2bbb642e6071b833238c32
ibrahim mohamed <ibrahimelstuhy@gmail.com>
2024-08-18 18:15:45 +0200

    insert modules

---
+++ payment application/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "platform_types.h"
+int main()
+{
+
+    return 0;
+}
---
+++ payment application/platform_types.h
@@ -0,0 +1,60 @@
+#ifndef PLATFORM_TYPES_H_INCLUDED
+#define PLATFORM_TYPES_H_INCLUDED
+#define CPU_TYPE_8 8
+#define CPU_TYPE_16 16
+#define CPU_TYPE_32 32
+#define CPU_TYPE_64 64
+
+#define CPU_TYPE (CPU_TYPE_64)
+
+#if(CPU_TYPE==CPU_TYPE_64)
+typedef unsigned char boolean;
+typedef unsigned char unit8;
+typedef unsigned short unit16;
+typedef unsigned int unit32;
+typedef unsigned long long unit64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+#elif(CPU_TYPE==CPU_TYPE_32)
+typedef unsigned char boolean;
+typedef unsigned char unit8;
+typedef unsigned short unit16;
+typedef unsigned long unit32;
+typedef unsigned long long unit64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+
+#elif(CPU_TYPE==CPU_TYPE_16)
+typedef unsigned char boolean;
+typedef unsigned char unit8;
+typedef unsigned short unit16;
+typedef unsigned long unit32;
+typedef unsigned long long unit64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+
+#elif(CPU_TYPE==CPU_TYPE_8)
+typedef unsigned char boolean;
+typedef unsigned char unit8;
+typedef unsigned short unit16;
+typedef unsigned long unit32;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef float float32;
+#endif
+
+
+#endif // PLATFORM_TYPES_H_INCLUDED
